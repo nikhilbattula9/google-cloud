4e6b53a5a539c424784037859590281fdcc7d5d4
nikhil babu battula <nikhilbattula9@gmail.com>
2023-05-02 00:48:37 -0300

    Testing ui

--- app/app.py
+++ app/app.py
@@ -1,7 +1,16 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/hello")
+# def hello_world():
+#     return "Demo Purpose"
+
+
 from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/hello")
-def hello_world():
-    return "Demo Purpose"
+app = Flask('hello-world')
+@app.route('/')
+def hello():
+ return "Welcome to  GKE\n"
+if __name__ == '__main__':
+ app.run(host = '0.0.0.0', port = 8080)
