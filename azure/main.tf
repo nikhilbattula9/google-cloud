d2ceaea6fb910a3a8ea723f9fbbe4a2527e934f3
nikhil babu battula <nikhilbattula9@gmail.com>
2023-11-27 10:25:07 -0500

    azure

--- azure/main.tf
+++ azure/main.tf
@@ -2,7 +2,6 @@
 resource "azurerm_resource_group" "rg" {
   name     = "Azure-09"
   location = "East US"
-  tags = { Environment = Test}
 }
 
 resource "azurerm_storage_account" "obsrv_st" {
@@ -11,5 +10,4 @@ resource "azurerm_storage_account" "obsrv_st" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
-  tags                     = azurerm_resource_group.rg.tags
 }
